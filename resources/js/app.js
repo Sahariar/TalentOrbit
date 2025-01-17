191b88f278fd73487108a584c3b2bb8740ca5c7a
Sahariar Kabir <sronok@gmail.com>
2025-01-18 05:00:20 +0600

    intregated dashboard and front end ui with js,css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,131 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import { createPopper } from '@popperjs/core';
+import Choices from 'choices.js';
+import GLightbox from 'glightbox';
+import Gumshoe from 'gumshoejs';
+import Masonry from 'masonry-layout';
+import noUiSlider from 'nouislider';
+import { lighten } from 'polished';
+import SimpleBar from 'simplebar';
+import SmoothScroll from 'smooth-scroll';
+import Swiper from 'swiper';
+
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+
+
+
+/********************* Menu Js **********************/
+
+//  Window scroll sticky class add
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if (
+        document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50
+    ) {
+        navbar.classList.add( "shadow", "nav-sticky");
+    } else {
+        navbar.classList.remove( "shadow", "nav-sticky");
+    }
+}
+
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+})
+
+
+//menu active & scrollspy
+var sections = document.querySelectorAll('.section');
+window.onscroll = function () {
+    var scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+    for (var elem in sections) {
+        if (sections.hasOwnProperty(elem) && (sections[elem].offsetTop - 105) <= scrollPos) {
+            var id = sections[elem].id;
+            if (id) {
+                document.querySelector('#navbar .navbar-nav .active').classList.remove('active');
+                document.querySelector('#navbar .navbar-nav .nav-link[href*=' + id + ']').parentNode.classList.add('active');
+            }
+        }
+    }
+};
+
+function initActiveMenu() {
+    var currentPath = location.pathname == "/" ? "index.html" : location.pathname.substring(1);
+    currentPath = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+    if (currentPath) {
+        var a = document.getElementById("navigation-menu").querySelector('li a[href="' + currentPath + '"]');
+        if (a) {
+            a.classList.add("active");
+            var parentCollapseDiv = a.closest(".dropdown-menu");
+            if (parentCollapseDiv) {
+                if (parentCollapseDiv.parentElement.classList.contains('dropdown')) {
+                    parentCollapseDiv.parentElement.children[0].classList.add("active");
+                    console.log("test", parentCollapseDiv.parentElement)
+                    if (parentCollapseDiv.parentElement.classList.contains('dropdown')) {
+                        parentCollapseDiv.parentElement.children[0].classList.add("active");
+                    }
+                }
+            }
+        }
+    }
+}
+
+// navbar toggler
+document.getElementById("navbar").querySelector(".navbar-toggler").addEventListener("click", function () {
+    var getAttr = this.getAttribute("data-collapse-toggle");
+    if (document.getElementById(getAttr).classList.contains("hidden")) {
+        document.getElementById(getAttr).classList.remove("hidden");
+    } else {
+        document.getElementById(getAttr).classList.add("hidden");
+    }
+});
+
+window.addEventListener('resize', function(){
+    document.getElementById('navbar-collapse').classList.add("hidden");
+});
+
+
+initActiveMenu();
+
+
+/************** Testimonial Slider *************/
+
+var swiper = new Swiper(".testimonialSlider", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        992: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        }
+    }
+});
+
+var swiper = new Swiper(".homeslider", {
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'TalentOrbit') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <link rel="icon" type="image/png" href="{{ asset('storage/images/talentorbit-fav.png') }}">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@4.2.0/css/line.min.css
+" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css','resources/css/public.css','resources/css/icons.min.css'])
+</head>
+
+<body class="font-sans antialiased bg-white">
+    <div class="min-h-screen ">
+        @include('layouts.pb-header')
+
+        <main class="main-content">
+            @yield('content')
+        </main>
+    </div>
+
+    @include('layouts.pb-footer')
+    <script src="https://unicons.iconscout.com/release/v4.0.0/script/monochrome/bundle.js"></script>
+    @vite(['resources/js/app.js'])
+</body>
+
 </html>
