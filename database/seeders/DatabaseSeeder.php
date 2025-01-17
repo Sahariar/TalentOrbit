4f4c373f05bc0135efeeda3e057ad104aba625a6
Saif Kamal <kamal.saifkamal534@gmail.com>
2025-01-17 02:25:19 -0800

    Database Table Migrations, Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\{User,CompanyProfile,CandidateProfile};
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\JobPost;
+use App\Models\Payment;
+use App\Models\PricingPlan;
+use App\Models\Tag;
+use Database\Factories\CandidateProfileFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +19,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)->create();
+        CompanyProfile::factory(5)->create();
+        CandidateProfile::factory(5)->create();
+        JobPost::factory(25)->create();
+        Category::factory(10)->create();
+        Tag::factory(10)->create();
+        Payment::factory(2)->create();
+        PricingPlan::factory(2)->create();
     }
 }
