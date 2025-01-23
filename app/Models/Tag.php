c4fdf04c7f37dd2b85b34ad7cfffe6a26f73d524
Saif Kamal <kamal.saifkamal534@gmail.com>
2025-01-23 01:58:56 -0800

    Removed Intermediate Tables, Added Notifiable & Many to Many Relationships in Model,Pricing Plan Schema Changed

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -11,4 +11,9 @@ class Tag extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function job_posts()
+    {
+        return $this->belongsToMany(JobPost::class);
+    }
 }
