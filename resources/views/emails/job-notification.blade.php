ce06e4c61410b4ed6736139b1af5128c15129b3d
Saif Kamal <kamal.saifkamal534@gmail.com>
2025-01-31 02:31:21 -0800

    Email Template Changed

--- resources/views/emails/job-notification.blade.php
+++ resources/views/emails/job-notification.blade.php
@@ -4,7 +4,7 @@
     <title>Job Notification</title>
 </head>
 <body>
-    <h1>New Job Posted in Your Category {{ category }}</h1>
+    <h1>New Job Posted in Your Category </h1>
     <p>Dear {{ $candidate->name }},</p>
     <p>We have a new job posted in the category you are subscribed to.</p>
     <p>Job Title: {{ $job->title }}</p>
